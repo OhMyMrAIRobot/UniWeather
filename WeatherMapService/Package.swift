f4fee5f1a629e9e761753a001d8214a1f982a9fd
Daniil Khlyshchankou <gfhieemobilelegends@gmail.com>
2025-04-27 18:18:41 +0300

    Added: dependencies between packages

--- WeatherMapService/Package.swift
+++ WeatherMapService/Package.swift
@@ -5,18 +5,22 @@ import PackageDescription
 
 let package = Package(
     name: "WeatherMapService",
-    platforms: [.iOS(.v13)],
+    platforms: [.iOS(.v17)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "WeatherMapService",
             targets: ["WeatherMapService"]),
     ],
+    dependencies: [
+        .package(name: "WeatherService", path: "../WeatherService")
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "WeatherMapService"),
+            name: "WeatherMapService",
+            dependencies: ["WeatherService"]),
 
     ]
 )
--- WeatherService/Package.swift
+++ WeatherService/Package.swift
@@ -5,18 +5,22 @@ import PackageDescription
 
 let package = Package(
     name: "WeatherService",
-    platforms: [.iOS(.v13)],
+    platforms: [.iOS(.v17)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "WeatherService",
             targets: ["WeatherService"]),
     ],
+    dependencies: [
+        .package(name: "APIClient", path: "../APIClient")
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "WeatherService"),
+            name: "WeatherService",
+            dependencies: ["APIClient"]),
 
     ]
 )
